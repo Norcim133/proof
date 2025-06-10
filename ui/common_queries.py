757d94b8c78592d1a8f77307dd35ed097594be14
Norcim133 <ian.hill@gmail.com>
2025-06-10 17:06:57 +1000

    Add graphic heavy first button

--- ui/common_queries.py
+++ ui/common_queries.py
@@ -4,6 +4,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def custom_button_0():
+    frame = inspect.currentframe()
+    key = frame.f_code.co_name
+    logger.info(f"{key}")
+    query = "Show budget trends"
+    if st.button(query, use_container_width=True, key=key):
+        st.session_state.common_prompt = query
+
 def custom_button_1():
     frame = inspect.currentframe()
     key = frame.f_code.co_name
@@ -39,6 +47,7 @@ def common_queries():
     st.subheader("Common Queries")
     st.text("")
 
+    custom_button_0()
     custom_button_1()
     custom_button_2()
     custom_button_3()
